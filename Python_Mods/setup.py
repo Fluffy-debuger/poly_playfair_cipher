ce862d49b4e0c500f5b2c6694ab7852b92c07d49
Fluffy-debuger <115883267+Fluffy-debuger@users.noreply.github.com>
2023-09-16 19:51:25 +0530

    Add files via upload

---
+++ Python_Mods/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='poly_playfair_cipher',
+    version='0.1',
+    description='Playfair cipher',
+    long_description='Playfair cipher Implementation in python',
+    author='Shailesh',
+    author_email='a69247199@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+)
